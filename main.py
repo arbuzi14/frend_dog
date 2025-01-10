f1e9d350461ac2140f33e9dce8cd0f560b00ceb4
arbuzik144656 <artales888@mail.ru>
2025-01-10 18:21:25 +0500

    WuW сушка кода

--- main.py
+++ main.py
@@ -7,19 +7,15 @@ def download_picture(folder, filename, url):
     file_path = f"{folder}/{filename}"
     response = requests.get(url)
     response.raise_for_status()
-    picture = response.content
     with open(file_path, 'wb') as file:
-        file.write(picture)
+        file.write(response.content)
 
 
-if __name__ == '__main__':
+def main():
     folder = "dogs"
-    if not os.path.isdir(folder):
-        os.mkdir(folder)
-    else:
-        shutil.rmtree(f"{folder}")
-        os.mkdir(folder)
-
+    if os.path.isdir(folder):
+        shutil.rmtree(folder)
+    os.mkdir(folder)
     for number in range(50):
         payload = {"filter": "mp4,webm"}
         url = 'https://random.dog/woof.json'
@@ -29,3 +25,7 @@ if __name__ == '__main__':
         link, picture_extension = os.path.splitext(picture_link)
         filename = f"dog_{number + 1}{picture_extension}"
         download_picture(folder, filename,  picture_link)
+
+
+if __name__ == '__main__':
+    main()
